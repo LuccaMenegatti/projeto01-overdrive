a9439c02badb682c7233b07e12a3fac6d42c5798
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-11 22:48:47 -0300

    Criando Login usuario

---
+++ login_user.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="estilos/StyleLogin.css">
+    <link rel="stylesheet" href="estilos/media-querie.css">
+    <title>CRUD - Overdrive</title>
+</head>
+<body>
+    <main>
+        <section id="login">
+            <div id="formulario">
+                <div class="logo">
+                    <img src="imagens/logo.png" class="logo" alt="Logo Overdrive">
+                </div>
+                <p>Seja bem vindo(a) ao projeto CRUD, insira seus dados para continuar:</p>
+                <form action="login.php" method="post" autocomplete="on">
+                    <div class="campo">
+                        <i class="material-icons">person</i>
+                        <input type="email" name="login" id="ilogin" placeholder="Insira seu CPF" 
+                        autocomplete="email" required maxlength="30"> 
+                        <label for="ilogin">Login</label>
+                    </div>
+                    <div class="campo">
+                        <i class="material-icons">vpn_key</i>
+                        <input type="password" name="senha" id="isenha" placeholder="Insira sua senha" 
+                        autocomplete="current-password" required minlength="8"> 
+                        <label for="isenha">Senha</label>
+                    </div>
+                    <input type="submit" value="Entrar"> 
+                    <a href="index.php" class="botao">Logar como ADM</a>
+                </form>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
