4471488db51ebeaa18bee11062508bff229f321e
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-19 01:11:57 -0300

    perfil usuario - background

--- restrito/acesso.php
+++ restrito/acesso.php
@@ -4,15 +4,13 @@
   Class Acesso {
     public static function validaUsuario($id_usuario) {
       
-    
-      // obter os dados do usuario em questão (id recebido pro parametro)
       $user_infos = "SELECT * FROM usuarios WHERE cod_pessoa = $id_usuario";
       $user_infos = mysqli_query($GLOBALS['conn'], $user_infos);
       $user_infos = mysqli_fetch_assoc($user_infos);
 
       $user_cpf = $user_infos['cpf'];
 
-      // verificar se este usuario está incluso na tablea de administradores do sistema
+      
       $verifica_usuario = "SELECT * FROM user_adm WHERE login = $user_cpf";
       $verifica_usuario = mysqli_query($GLOBALS['conn'], $verifica_usuario);
 
