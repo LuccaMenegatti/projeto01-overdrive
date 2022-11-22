d3dee4b7a9c3be5402fe61cfe49f814d53708cb6
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-22 08:19:49 -0300

    Script modal

--- restrito/pesquisar_empresa.php
+++ restrito/pesquisar_empresa.php
@@ -104,7 +104,8 @@
                   <a href='#' class='botao4' data-bs-toggle='modal' data-bs-target='#confirma' onclick="<?php echo "pegar_dados('$nome', '$cod_company')" ?>">Excluir</a>
                 </td>
               </tr>
-              <!-- -=-=-=-=-=-=-=-=-=- BOTÃO CONFIRMAÇÃO -=-=-=-=-=-=-=-=-=- -->
+             
+              
               <div class="modal fade" id="confirma" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
@@ -144,12 +145,6 @@
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
 
   </main>
 </body>
