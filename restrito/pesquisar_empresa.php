d57803655e85f74a6c7607ed871775b8abd0427a
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-22 08:14:42 -0300

    Estilizando pesquisas

--- restrito/pesquisar_empresa.php
+++ restrito/pesquisar_empresa.php
@@ -12,10 +12,12 @@
 <html lang="pt-br">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="images/icons/icone-logo.png">
+  <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="estilo_restrito/styleindex.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -24,6 +26,19 @@
 </head>
 
 <body>
+
+<header>
+      <div class="logo">
+        <img src="../imagens/logo.png" class="logo" alt="Logo Overdrive">
+      </div>
+</header>
+<nav>
+    <a href="index.php"> Voltar</a>
+    <a href="../logout.php">Sair</a>
+    <a href="pesquisar_empresa.php"> <i class="material-icons">search</i> Empresas</a>
+    <a href="pesquisa.php"><i class="material-icons">search</i> Usuarios</a>
+</nav>
+
   <?php
 
   $pesquisa = $_POST['busca'] ?? ''; 
@@ -37,21 +52,17 @@
   ?>
 
 
-
+<main>
   <div class="container">
     <div class="row">
       <div class="col">
-        <h1>Pesquisar</h1>
 
-        <!-- -=-=-=-=-=-=-=-=-=- NAVBAR -=-=-=-=-=-=-=-=-=--->
-        <nav class="navbar navbar-light bg-light">
-          <div class="container-fluid">
+        <div id="divBusca">
             <form class="d-flex" action="pesquisar_empresa.php" method="POST">
-              <input class="form-control me-2" type="search" placeholder="Nome" aria-label="Search" name="busca" autofocus>
-              <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+              <input class="form-control me-2" type="search" placeholder="Buscar empresa..." aria-label="Search" name="busca" id="txtBusca" autofocus>
+              <button class="botao1" type="submit"><i class="material-icons">search</i></button>
             </form>
           </div>
-        </nav>
 
         <!-- -=-=-=-=-=-=-=-=-=- TABELA -=-=-=-=-=-=-=-=-=- -->
         <table class="table table-hover">
@@ -66,9 +77,7 @@
               <th scope="col">Funções</th>
             </tr>
           </thead>
-          <!-- -=-=-=-=-=-=-=-=-=- PUXANDO OS DADOS DO BANCO -=-=-=-=-=-=-=-=-=- -->
           <tbody>
-
             <?php
 
             while ($linha = mysqli_fetch_assoc($dados)) {
@@ -81,8 +90,8 @@
               $responsavel = $linha['responsavel'];
               $sql_responsavel = "SELECT * FROM user_adm";
               $exec = mysqli_query($conn, $sql_responsavel);
-             
             ?>
+
               <tr>
                 <th scope='row'><?php echo $nome ?></th>
                 <td><?php echo $nome_fantasia ?></td>
@@ -91,8 +100,8 @@
                 <td><?php echo $telefone ?></td>
                 <td><?php echo $responsavel?></td>
                 <td width=150px>
-                  <a href='cadastro_empresa_edit.php?id=<?php echo $cod_company ?>' class='btn btn-success btn-sm'>Editar</a>
-                  <a href='#' class='btn btn-danger btn-sm' data-bs-toggle='modal' data-bs-target='#confirma' onclick="<?php echo "pegar_dados('$nome', '$cod_company')" ?>">Excluir</a>
+                  <a href='cadastro_empresa_edit.php?id=<?php echo $cod_company ?>' class='botao3'>Editar</a>
+                  <a href='#' class='botao4' data-bs-toggle='modal' data-bs-target='#confirma' onclick="<?php echo "pegar_dados('$nome', '$cod_company')" ?>">Excluir</a>
                 </td>
               </tr>
               <!-- -=-=-=-=-=-=-=-=-=- BOTÃO CONFIRMAÇÃO -=-=-=-=-=-=-=-=-=- -->
@@ -100,17 +109,17 @@
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Confirmação de exclusão</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">Confirmação</h5>
                     </div>
                     <div class="modal-body">
                       <form action="excluir_empresa_script.php" method="POST">
-                        <p>Tem certeza que quer Excluir essa Empresa? <b id="nome">Nome da Empresa</b>?</p>
+                        <p>Tem certeza que quer Excluir essa Empresa?</p>
                     </div>
                     <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não</button>
+                      <button type="button" class="botao3" data-bs-dismiss="modal">Não</button>
                       <input type="hidden" name="nome" id="nome_empre_1" value="<?= $nome_fantasia ?>" />
                       <input type="hidden" name="id" id="cod_company" value="<?php echo $cod_company ?>" />
-                      <input type="submit" class="btn btn-danger" value="Sim">
+                      <input type="submit" class="botao4" value="Sim">
                       </form>
                     </div>
                   </div>
@@ -119,12 +128,6 @@
             <?php } ?>
           </tbody>
         </table>
-
-        <!-- -=-=-=-=-=-=-=-=-=- BOTÃO -=-=-=-=-=-=-=-=-=- -->
-        <div class="mb-3">
-          <a href="index.php" class="btn btn-outline-info"> Voltar para o Inicio</a>
-        </div>
-
       </div>
     </div>
   </div>
@@ -147,6 +150,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
 
+  </main>
+</body>
 </html>
