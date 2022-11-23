53f008804e3db80e773504f84991d0c62abdeda2
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-23 16:46:45 -0300

    media-queries para botões

--- restrito/pesquisa.php
+++ restrito/pesquisa.php
@@ -16,6 +16,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="estilo_restrito/styleindex.css">
+  <link rel="stylesheet" href="estilo_restrito/media-querie2.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
