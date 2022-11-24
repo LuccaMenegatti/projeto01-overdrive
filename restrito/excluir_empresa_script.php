2b61c74a4b238c5903afbd04872d70298728a901
Lucca Antonio Menegatti <117111937+LuccaMenegatti@users.noreply.github.com>
2022-11-24 08:21:44 -0300

    Exclusão de empresas

--- restrito/excluir_empresa_script.php
+++ restrito/excluir_empresa_script.php
@@ -5,15 +5,34 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="images/icons/icone-logo.png">
-
+  <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="estilo_restrito/styleindex.css">
+  <link rel="stylesheet" href="estilo_restrito/media-querie2.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>Exclusão de Empresa</title>
+
+  <style>
+  body, html{
+      color: white;
+    }
+
+  </style>
 </head>
 
 <body>
+
+<header>
+      <div class="logo">
+        <img src="../imagens/logo.png" class="logo" alt="Logo Overdrive">
+      </div>
+</header>
+<nav>
+    <a href="pesquisar_empresa.php"> Voltar</a>
+</nav>
+<main>
   <div class="container">
     <div class="row">
       <?php
@@ -38,9 +57,9 @@
       }
 
       ?>
-      <a href="pesquisar_empresa.php" class="btn btn-primary">Voltar</a>
     </div>
   </div>
+</main>
 
 
 
--- restrito/pesquisar_empresa.php
+++ restrito/pesquisar_empresa.php
@@ -12,25 +12,17 @@
 <html lang="pt-br">
 
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="../imagens/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="estilo_restrito/styleindex.css">
   <link rel="stylesheet" href="estilo_restrito/media-querie2.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>Pesquisar Empresa</title>
-
-  <style>
-
-form.branco{
-  background-color: white;
-}
-
-</style>
 </head>
 
 <body>
@@ -45,7 +37,7 @@ form.branco{
     <a href="pesquisar_empresa.php"> <i class="material-icons">search</i> Empresas</a>
     <a href="pesquisa.php"><i class="material-icons">search</i> Usuarios</a>
 </nav>
-
+<main>
   <?php
 
   $pesquisa = $_POST['busca'] ?? ''; 
@@ -59,20 +51,18 @@ form.branco{
   ?>
 
 
-<main>
+
   <div class="container">
     <div class="row">
       <div class="col">
-
-        <div id="divBusca">
+        
             <form class="d-flex" action="pesquisar_empresa.php" method="POST">
-              <input class="form-control me-2" type="search" placeholder="Buscar empresa..." aria-label="Search" name="busca" id="txtBusca" autofocus>
+              <input class="form-control me-2" type="search" placeholder="Nome" aria-label="Search" name="busca" autofocus>
               <button class="botao1" type="submit"><i class="material-icons">search</i></button>
             </form>
-          </div>
-
+        
         <!-- -=-=-=-=-=-=-=-=-=- TABELA -=-=-=-=-=-=-=-=-=- -->
-        <div class="table">
+        
         <table class="table table-hover">
           <thead>
             <tr>
@@ -85,7 +75,9 @@ form.branco{
               <th scope="col">Funções</th>
             </tr>
           </thead>
+          <!-- -=-=-=-=-=-=-=-=-=- PUXANDO OS DADOS DO BANCO -=-=-=-=-=-=-=-=-=- -->
           <tbody>
+
             <?php
 
             while ($linha = mysqli_fetch_assoc($dados)) {
@@ -99,30 +91,33 @@ form.branco{
               $sql_responsavel = "SELECT * FROM user_adm";
               $exec = mysqli_query($conn, $sql_responsavel);
             ?>
-
               <tr>
                 <th scope='row'><?php echo $nome ?></th>
                 <td><?php echo $nome_fantasia ?></td>
                 <td><?php echo $cnpj ?></td>
                 <td><?php echo $endereco ?></td>
                 <td><?php echo $telefone ?></td>
-                <td><?php echo $responsavel?></td>
+                <td><?php echo $responsavel ?></td>
                 <td width=150px>
                   <a href='cadastro_empresa_edit.php?id=<?php echo $cod_company ?>' class='botao3'>Editar</a>
                   <a href='#' class='botao4' data-bs-toggle='modal' data-bs-target='#confirma' onclick="<?php echo "pegar_dados('$nome', '$cod_company')" ?>">Excluir</a>
                 </td>
               </tr>
-             
-              
-              <div class="modal fade" id="confirma" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <?php } ?>
+          </tbody>
+        </table>
+        </div>
+
+         <!-- -=-=-=-=-=-=-=-=-=- BOTÃO CONFIRMAÇÃO -=-=-=-=-=-=-=-=-=- -->
+         <div class="modal fade" id="confirma" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Confirmação</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">Confirmação de exclusão</h5>
                     </div>
                     <div class="modal-body">
-                      <form class="branco" action="excluir_empresa_script.php" method="POST">
-                        <p>Tem certeza que quer Excluir essa Empresa?</p>
+                      <form action="excluir_empresa_script.php" method="POST">
+                        <p>Tem certeza que quer Excluir essa Empresa? <b id="nome">Nome da Empresa</b>?</p>
                     </div>
                     <div class="modal-footer">
                       <button type="button" class="botao3" data-bs-dismiss="modal">Não</button>
@@ -134,10 +129,6 @@ form.branco{
                   </div>
                 </div>
               </div>
-            <?php } ?>
-          </tbody>
-        </table>
-        </div>
       </div>
     </div>
   </div>
@@ -155,6 +146,6 @@ form.branco{
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-  </main>
+</main>
 </body>
 </html>
